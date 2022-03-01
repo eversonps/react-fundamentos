e893ebe0906cd6f42e7ec4de48d83b9da666e284
Everson Pereira <eversonpereiraofc@gmail.com>
2022-03-01 17:06:17 -0300

    conhecendo e utilizando JSX e importando CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import "./index.css"
 import ReactDOM from "react-dom"
+import React from "react"
 
-const el = document.querySelector("#root")
-ReactDOM.render("Olá React!!!", el)
+const tag = <h1>Olá react</h1>
+ReactDOM.render(
+    <div>
+        {tag}
+    </div>, 
+    document.querySelector("#root")
+)
