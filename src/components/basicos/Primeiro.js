0a1a404460c0a2669c5ba808539a5fb8f1b2f5b8
Everson Pereira <eversonpereiraofc@gmail.com>
2022-03-01 17:19:51 -0300

    Criando primeiro componente

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const msg = "Seja bem vindo!"
+export default function Primeiro(){
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+            <p>{msg}</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./index.css"
 import ReactDOM from "react-dom"
 import React from "react"
+import Primeiro from "./components/basicos/Primeiro"
 
-const tag = <h1>Olá react</h1>
 ReactDOM.render(
     <div>
-        {tag}
+        <Primeiro></Primeiro>
     </div>, 
     document.querySelector("#root")
 )
