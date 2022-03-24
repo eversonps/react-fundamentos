428db12ba70fcc7e0621a1e23e541e3dd1de5cb9
Everson <eversonpereiraofc@gmail.com>
2022-03-24 16:01:43 -0300

    passando propriedades para componente filho

--- v4/.history/src/componentes/PrimeiroComponente_20220324160034.jsx
+++ v4/.history/src/componentes/PrimeiroComponente_20220324160034.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-let isLegal = false
 export default (props) => (
     <div>
         <h1>{props.valor}</h1>
         <h2>{1 + 1}</h2>
-        <p>{isLegal ? "Sim" : "Não"}</p>
+        <p>{true ? "Sim" : "Não"}</p>
     </div>
 )
 
--- v4/.history/src/index_20220324155905.js
+++ v4/.history/src/index_20220324155905.js
@@ -5,7 +5,7 @@ import PrimeiroComponente from "./componentes/PrimeiroComponente"
 const elemento = document.getElementById("root")
 ReactDOM.render(
     <div>
-        <PrimeiroComponente valor="Primeiro Componente!!!!"/>
+        <PrimeiroComponente valor="Primeiro Componente!!!!" aBcD={Math.pow(2, 8)/>
     </div>,
     elemento
 )
